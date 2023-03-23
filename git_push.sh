5d99203ce22f909234e734cad935a368b70747ad
NetAcad DEVASC <changeme@example.com>
2023-03-23 10:46:06 +0000

    "Skill Assessment 1. commit"

---
+++ git_push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# initializes git
+git init 
+
+# Adds everything
+git add *
+
+# Commit files
+git commit -m '"Skill Assessment 1. commit"'
+
+# Set remote origin
+git remote add origin https://github.com/kimx052t/DEVASC-Exam.git
+
+# Push master branch to online git repository
+git push origin master
+
